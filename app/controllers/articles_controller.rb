e6a4fcbe0e562d330eb2775b76e39665f9bd1f9b
hongdp <hongdp@gmail.com>
2014-11-23 01:00:20 -0500

    version 1.0

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+ 	def index
+ 		@articles = Article.all
+ 	end
+ 	
 	def new
 		@article = Article.new
 	end
@@ -22,12 +28,22 @@ class ArticlesController < ApplicationController
 
 	def edit
 		@article = Article.find(params[:id])
-		
+	end
+
+	def update
+		@article = Article.find(params[:id])
 		if @article.update(article_params)
     		redirect_to @article
 		else
 			render 'edit'
 		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		
+		redirect_to articles_path
 	end
 
 	private
