0d45df05e1c6231c5ea26624811f37d4d1eefcdf
hongdp <hongdp@gmail.com>
2014-11-30 13:47:44 -0500

    version 1.1
    add css

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,21 @@
 class CommentsController < ApplicationController
-	  http_basic_authenticate_with name: "hongdp", password: "233333", only: :destroy
+	http_basic_authenticate_with name: "hongdp", password: "233333", only: :destroy
  
-  def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
-  end
+ 	def create
+    	@article = Article.find(params[:article_id])
+    	@comment = @article.comments.create(comment_params)
+    	redirect_to article_path(@article)
+	end
  
-  def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article)
-  end
+	def destroy
+    	@article = Article.find(params[:article_id])
+    	@comment = @article.comments.find(params[:id])
+    	@comment.destroy
+    	redirect_to article_path(@article)
+	end
  
-  private
+private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+    	params.require(:comment).permit(:commenter, :body)
     end
 end
